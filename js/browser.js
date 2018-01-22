b38d4cf28b32797e40b1fcd52ec90ca8452ec284
Cole.yu <1032577646@qq.com>
2018-01-22 15:13:55 +0800

    userAgent机制
    修改了用户代理字符串检测机制

--- js/browser.js
+++ js/browser.js
@@ -7,15 +7,16 @@ var browser={
             presto: u.indexOf('Presto') > -1, //opera内核
             webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
             gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-            mobile: !!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/)&&u.indexOf('QIHU')&&u.indexOf('Chrome')<0, //是否为移动终端
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
             ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
             android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPhone: u.indexOf('iPhone') > -1, //是否为iPhone
             iPad: u.indexOf('iPad') > -1, //是否为iPad
             webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
-            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
-            };
-        }(),
+            weixin: u.indexOf('MicroMessenger') == -1， //是否为微信浏览器
+            qq: u.match(/\sQQ/i) == " qq" //是否为QQ
+        };
+    }(),
     language:(navigator.browserLanguage || navigator.language).toLowerCase() 
 }
 

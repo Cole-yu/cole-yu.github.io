2f8c6e9435ba6c3808eb7793673fbb176af89866
Cole.yu <1032577646@qq.com>
2019-03-03 23:41:24 +0800

    添加 favicon.ico 标题icon ,合并部分图片

--- app.968cb261.js
+++ app.968cb261.js
@@ -214,10 +214,10 @@ function change(doc, win) {
     var docEl = doc.documentElement,
         isIOS = navigator.userAgent.match(/iphone|ipod|ipad/gi),
         dpr = isIOS ? Math.min(win.devicePixelRatio, 3) : 1,
-        dpr = window.top === window.self ? dpr : 1,
-        dpr = 1, // 首页引用IFRAME，强制为1
+        dpr = window.top === window.self ? dpr : 1,         // 判断是否是顶层窗口
+        dpr = 1,                                            // 首页引用IFRAME，强制为1
         scale = 1 / dpr,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';     // 移动端翻转设备（即方向发生变化）时触发的事件 和 PC 端网页大小调整
     docEl.dataset.dpr = win.devicePixelRatio;
     if (navigator.userAgent.match(/iphone/gi) && screen.width == 375 && win.devicePixelRatio == 2) {
         docEl.classList.add('iphone6')
@@ -227,8 +227,8 @@ function change(doc, win) {
     }
     //添加一条元数据标签
     var metaEl = doc.createElement('meta');
-    metaEl.name = 'viewport';
-    metaEl.content = 'initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale;
+    metaEl.name = 'viewport';    
+    metaEl.content = 'width=device-width, initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ", shrink-to-fit=no";
     docEl.firstElementChild.appendChild(metaEl);
     function recalc() {
         var width = docEl.clientWidth;
